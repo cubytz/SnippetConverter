2a477c0e13b8e072289c1625be02fa2ecf364f96
Jeremie Henri <jeremie@cubytz.com>
2018-05-02 14:19:38 +0900

    Cleaned up code Clearer methods name Documentation blocs Specs

--- lib/snippet_converter.rb
+++ lib/snippet_converter.rb
@@ -14,6 +14,8 @@ require 'snippet_converter/version'
 class SnippetConverter
   include REXML
 
+  # variable referencing the corresponding
+  # keys {Sublime: 'VSC'}
   KEYS = {
     content: 'body',
     tabTrigger: 'prefix',
@@ -21,14 +23,28 @@ class SnippetConverter
     description: 'description'
   }.freeze
 
+  #
+  # Parse arguments
+  #
+  # @param [Array] args Array of arguments passed in cli
+  #
+  # @return [OpenStruct] options as a ostruct
+  #
   def self.parse(args)
     options = OpenStruct.new
     options.encoding = 'utf8'
     options.verbose = false
     options.split = false
-    options.output = 'snippets'
+    options.output = 'json'
+    options.dest = 'snippets'
 
     opts = OptionParser.new do |op|
+      op.separator ''
+      op.banner = "SnippetConverter, version #{VERSION}"
+      op.banner = "#{Gem.loaded_specs['snippet_converter'].license} License"
+      op.banner = 'Copyright (c) 2018 Jeremie Henri - Cubytz'
+
+      op.separator ''
       op.banner = "Usage:\tsnippetconverter.rb [options]\n\tFile(s) or directory required"
 
       op.separator ''
@@ -49,9 +65,14 @@ class SnippetConverter
         options.split = s
       end
 
-      # Output folder
-      op.on('-o', '--output output', 'Output directory [snippets]') do |o|
+      # Output
+      op.on('-o', '--output output', 'Output format (json)') do |o|
         options.output = o
+      end
+
+      # Destination folder
+      op.on('--destination destination', 'Destination directory (snippets)') do |dest|
+        options.destination = dest
       end
 
       # Verbose
@@ -79,15 +100,30 @@ class SnippetConverter
     options
   end
 
-  def self.convert(file)
+  #
+  # Read snippet file and return the first snippet found
+  #
+  # @param [String] file Snipppet file path
+  #
+  # @return [REXML::Element] First snippet element found
+  #
+  def self.read_snippet(file)
     xmlfile = File.new(file)
     doc = Document.new(xmlfile)
 
-    # Now get the root element
-    root = doc.root
+    # return the snippet element
+    XPath.first(doc.root, '//snippet')
+  end
 
-    # Info for the first snippet found
-    snippet_element = XPath.first(root, '//snippet')
+  #
+  # Creates a Hash element with VSC keys and
+  # formatted body
+  #
+  # @param [REXML::Element] snippet_element Snippet xml element
+  #
+  # @return [Hash] Snippet as a hash
+  #
+  def self.create_json_snippet(snippet_element)
     snippet = {}
     snippet_element.elements.each do |element|
       key = KEYS[element.name.to_sym]
@@ -103,14 +139,44 @@ class SnippetConverter
     snippet
   end
 
+  #
+  # Convert file content to VSC snippet format
+  #
+  # @param [file] file Snippet file path
+  #
+  # @return [Hash] VSC Json snippet
+  #
+  def self.convert_to_vsc(file)
+    # Get the first snippet element
+    snippet_element = read_snippet(file)
+    # Convert the snippet to VSC snippet format
+    create_json_snippet(snippet_element)
+  end
+
+  #
+  # Format the body element
+  #
+  # @param [String] body Sublime snippet content element
+  #
+  # @return [Array] Body as an array
+  #
   def self.format_body(body)
+    # Split by \n
     formatted_body = body.split("\n")
+    # Remove any empty element from the array
     formatted_body.delete('')
     formatted_body
   end
 
-  def self.output_file(file, snippet)
+  #
+  # Save snippet content to disk
+  #
+  # @param [String] file filename
+  # @param [Has] snippet snippet hash
+  #
+  def self.write_file(file, snippet)
     File.open(file, 'w') do |f|
+      # Pretty generate the JSON before saving
       f.write(JSON.pretty_generate(snippet))
     end
   end
--- spec/snippet_converter_spec.rb
+++ spec/snippet_converter_spec.rb
@@ -1,21 +1,28 @@
 RSpec.describe SnippetConverter do
-  it 'has a version number' do
-    expect(SnippetConverter::VERSION).not_to be nil
+  context 'version' do
+    it 'has a version number' do
+      expect(SnippetConverter::VERSION).not_to be nil
+    end
   end
 
   context 'methods' do
     it 'responds to parse' do
       expect(SnippetConverter).to respond_to('parse')
     end
-    it 'responds to convert' do
-      expect(SnippetConverter).to respond_to('convert')
+    it 'responds to convert_to_vsc' do
+      expect(SnippetConverter).to respond_to('convert_to_vsc')
     end
     it 'responds to format_body' do
       expect(SnippetConverter).to respond_to('format_body')
     end
-    it 'responds to output_file' do
-      expect(SnippetConverter).to respond_to('output_file')
+    it 'responds to write_file' do
+      expect(SnippetConverter).to respond_to('write_file')
+    end
+    it 'responds to create_json_snippet' do
+      expect(SnippetConverter).to respond_to('create_json_snippet')
+    end
+    it 'responds to read_snippet' do
+      expect(SnippetConverter).to respond_to('read_snippet')
     end
   end
-
 end
